7ea06f27ce68e8a6ebd66513c0747ede2f2b2e5c
Meghan Debity <mdebity@gmail.com>
2018-07-06 09:29:11 -0500

    first commit

---
+++ lightening-01.py
@@ -0,0 +1,13 @@
+# Write a function that takes a list, a number, and a string as args. 
+def function(str, number, list):
+    myList = ["happy", "days", "when", "dinos", "walked"]
+    print("This argument:", str)
+    print("This number:", 2)
+    print("This is a list:", myList)
+
+# The string parameter should have a default value. 
+# In the function body, loop over the list and output the items.
+# Use slice to loop through only the first n items in the array, where n = the value of the second argument.
+# Each item should be prefaced by value of the string argument
+# One example output might then be "I have visited the city of San Francisco" if "San Francisco" was an item in the list, and the string argument was "I have visited the city of "
+# Try it out! Execute the function both with and without passing in a value for the string parameter
